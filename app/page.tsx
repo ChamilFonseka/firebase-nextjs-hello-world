0c8771db6cec8cb26e2310b5e29cc74bb21f3428
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-12-26 10:27:21 +0530

    removed tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex justify-center items-center flex-col gap-5 bg-sky-200 text-sky-500">
-      <h1 className="text-5xl font-black tracking-wide">{process.env.TITLE}</h1>
+      <h1 className="text-5xl font-black tracking-wide">
+        Hello Next.js on Firebase App Hosting!
+      </h1>
       <p className="text-lg italic underline">
-        {process.env.SUBTITLE}
+        This is a Next.js app hosted on Firebase App Hosting
       </p>
     </main>
   );
